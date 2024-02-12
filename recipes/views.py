a42f6194837cf3e9786292b48f80b26f97e8ab16
Nickolas Dos Santos Cremasco <nickolascremasco@gmail.com>
2024-02-12 16:19:19 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'global/home.html', context={
+    return render(request, 'recipes/home.html', context={
         'name':'Nickolas'
     })
-
-def contato(request):
-    return render(request, 'temp/temp.html')
-
-def sobre(request):
-    return HttpResponse('eae')
