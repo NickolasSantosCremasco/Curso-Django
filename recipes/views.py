81bf097036ac5a6da7e48500007fbca798149f3c
Nickolas Dos Santos Cremasco <nickolascremasco@gmail.com>
2024-02-20 21:13:01 -0300

    fix the card

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-
+from utils.recipes.factory import make_recipe
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name':'Nickolas'
+        'recipes':[make_recipe() for _ in range(10)],
     })
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name':'Nickolas'
+        'recipe':make_recipe(),
+        'is_detail_page':True,
     })
