2dc695bdf4639c58225626bb46591bfb95f34ddf
Nickolas Dos Santos Cremasco <nickolascremasco@gmail.com>
2024-02-22 21:04:01 -0300

    creating-superuser-model-category-puted

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . models import Category
+from .models import Recipe
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+
+admin.site.register(Category, CategoryAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # the class is like one table in databases
+
+
+class Category(models.Model):
+    name = models.CharField(max_length = 65)
+
+    def __str__(self):
+        return self.name
+
+
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -10,11 +20,16 @@ class Recipe(models.Model):
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
-    
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now = True)
     is_published = models.BooleanField(default = False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null = True
+    )
+    author = models.ForeignKey(
+        User , on_delete = models.SET_NULL, null = True
+    )
 
 
 
